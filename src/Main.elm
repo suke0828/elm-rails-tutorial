f66419b561467506a9c7e4eb7a9baeccb8b01f8f
suke0828 <mwbspa@gmail.com>
2023-01-23 10:23:35 +0900

    ♻️ Add helper method header and footer
    headerとfooterの補助関数を作成して、ページごとにタイトルが変更されるようにした
    
    issue #7

--- src/Main.elm
+++ src/Main.elm
@@ -130,68 +130,102 @@ subscriptions _ =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Home | Ruby on Rails Tutorial Sample App"
-    , body =
-        -- Header
-        [ nav [ attribute "aria-label" "main navigation", class "navbar is-black  is-fixed-top", attribute "role" "navigation" ]
-            [ div [ class "navbar-brand" ]
-                [ a [ class "navbar-item", id "logo", href "/" ] [ text "Sample App" ]
-                , div [ class "navbar-burger", attribute "data-target" "navMenu" ]
-                    [ span []
-                        []
-                    , span []
-                        []
-                    , span []
-                        []
-                    ]
+    -- Container
+    case model.page of
+        HomePage ->
+            { title = "Home | Ruby on Rails Tutorial Sample App"
+            , body =
+                [ headerView
+                , StaticPages.Home.view
+                , footerView
                 ]
-            , div [ class "navbar-menu" ]
-                [ div [ class "navbar-start" ] []
-                , div [ class "navbar-end" ]
-                    [ viewLink "home" "Home"
-                    , viewLink "help" "Help"
-                    , viewLink "login" "Log in"
-                    ]
+            }
+
+        AboutPage ->
+            { title = "About | Ruby on Rails Tutorial Sample App"
+            , body =
+                [ headerView
+                , StaticPages.About.view
+                , footerView
                 ]
-            ]
+            }
 
-        -- Container
-        , case model.page of
-            HomePage ->
-                StaticPages.Home.view
-
-            AboutPage ->
-                StaticPages.About.view
-
-            HelpPage ->
-                StaticPages.Help.view
-
-            ContactPage ->
-                StaticPages.Contact.view
-
-            SignUpPage ->
-                Users.New.view
-
-        -- Footer
-        , footer []
-            [ small []
-                [ text "The "
-                , a [ href "https://railstutorial.jp/" ] [ text "Ruby on Rails Tutorial" ]
-                , text " By "
-                , a [ href "https://www.michaelhartl.com/" ] [ text "Michael Hartl" ]
+        HelpPage ->
+            { title = "Help | Ruby on Rails Tutorial Sample App"
+            , body =
+                [ headerView
+                , StaticPages.Help.view
+                , footerView
                 ]
-            , nav []
-                [ ul []
-                    [ li [] [ a [ href "about" ] [ text "About" ] ]
-                    , li [] [ a [ href "contact" ] [ text "Contact" ] ]
-                    , li [] [ a [ href "https://news.railstutorial.org/" ] [ text "News" ] ]
-                    ]
+            }
+
+        ContactPage ->
+            { title = "Contact | Ruby on Rails Tutorial Sample App"
+            , body =
+                [ headerView
+                , StaticPages.Contact.view
+                , footerView
                 ]
-            ]
-        ]
-    }
+            }
+
+        SignUpPage ->
+            { title = "SignUp | Ruby on Rails Tutorial Sample App"
+            , body =
+                [ headerView
+                , Users.New.view
+                , footerView
+                ]
+            }
+
+
+
+-- 補助関数
 
 
 viewLink : String -> String -> Html msg
 viewLink path name =
     a [ class "navbar-item", href path ] [ text name ]
+
+
+headerView : Html msg
+headerView =
+    nav [ attribute "aria-label" "main navigation", class "navbar is-black  is-fixed-top", attribute "role" "navigation" ]
+        [ div [ class "navbar-brand" ]
+            [ a [ class "navbar-item", id "logo", href "/" ] [ text "Sample App" ]
+            , div [ class "navbar-burger", attribute "data-target" "navMenu" ]
+                [ span []
+                    []
+                , span []
+                    []
+                , span []
+                    []
+                ]
+            ]
+        , div [ class "navbar-menu" ]
+            [ div [ class "navbar-start" ] []
+            , div [ class "navbar-end" ]
+                [ viewLink "home" "Home"
+                , viewLink "help" "Help"
+                , viewLink "login" "Log in"
+                ]
+            ]
+        ]
+
+
+footerView : Html msg
+footerView =
+    footer []
+        [ small []
+            [ text "The "
+            , a [ href "https://railstutorial.jp/" ] [ text "Ruby on Rails Tutorial" ]
+            , text " By "
+            , a [ href "https://www.michaelhartl.com/" ] [ text "Michael Hartl" ]
+            ]
+        , nav []
+            [ ul []
+                [ li [] [ a [ href "about" ] [ text "About" ] ]
+                , li [] [ a [ href "contact" ] [ text "Contact" ] ]
+                , li [] [ a [ href "https://news.railstutorial.org/" ] [ text "News" ] ]
+                ]
+            ]
+        ]

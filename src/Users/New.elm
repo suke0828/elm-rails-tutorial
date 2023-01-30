2fb111ba85f47ab6478808d09c97a3027ec0e388
suke0828 <mwbspa@gmail.com>
2023-01-30 12:44:07 +0900

    ✨ Add Form and User model
    ・name属性とemail属性の追加
    ・バリデーションを判定するStatusを作成
    ・Formの作成
    ・Formが空欄だった場合のバリデーションとエラーメッセージを作成
    
    issue #9

--- src/Users/New.elm
+++ src/Users/New.elm
@@ -2,11 +2,123 @@ module Users.New exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 
 
-view : Html msg
-view =
+
+-- Model
+
+
+type alias Model =
+    { name : String
+    , nameValidation : NameStatus
+    , email : String
+    , emailValidation : EmailStatus
+    }
+
+
+type NameStatus
+    = EmptyName
+    | ValidName
+
+
+type EmailStatus
+    = EmptyEmail
+    | ValidEmail
+
+
+
+-- Init
+
+
+init : ( Model, Cmd msg )
+init =
+    ( { name = "Tom"
+      , nameValidation = ValidName
+      , email = "aaa@email.jp"
+      , emailValidation = ValidEmail
+      }
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = Name String
+    | Email String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Name name ->
+            ( validate { model | name = name }, Cmd.none )
+
+        Email email ->
+            ( validate { model | email = email }, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
     div []
         [ h1 [] [ text "Sign up" ]
         , p [] [ text "This will be a signup page for new users." ]
+        , input [ type_ "text", placeholder "Name", value model.name, onInput Name ] []
+        , nameError model.nameValidation
+        , input [ type_ "text", placeholder "Email", value model.email, onInput Email ] []
+        , emailError model.emailValidation
         ]
+
+
+
+-- Validation
+
+
+validate : Model -> Model
+validate model =
+    let
+        nameStatus =
+            if model.name == "" then
+                EmptyName
+
+            else
+                ValidName
+
+        emailStatus =
+            if model.email == "" then
+                EmptyEmail
+
+            else
+                ValidEmail
+    in
+    { model
+        | nameValidation = nameStatus
+        , emailValidation = emailStatus
+    }
+
+
+
+-- Error Message
+
+
+nameError : NameStatus -> Html msg
+nameError status =
+    case status of
+        ValidName ->
+            div [ style "color" "green" ] [ text "OK" ]
+
+        EmptyName ->
+            div [ style "color" "red" ] [ text "Name can't be blank" ]
+
+
+emailError : EmailStatus -> Html msg
+emailError status =
+    case status of
+        ValidEmail ->
+            div [ style "color" "green" ] [ text "OK" ]
+
+        EmptyEmail ->
+            div [ style "color" "red" ] [ text "Email can't be blank" ]
